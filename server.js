898fcf0d7ff797994384910fc63b9e9bd9439b05
Devang  Tyagi <dev@Devangs-MacBook.local>
2018-06-06 00:40:42 +0530

    test changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http= require('http');
 const app=require('./app');
-const port =process.env.PORT||3000;
+const port =process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
 const server= http.createServer(app);
-server.listen(port);
-console.log("server is listening to "+port);
+server.listen(port,address);
+console.log("server is listening to "+address +port);
