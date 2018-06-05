bbcda577a478c4ce8607ed5ed4c924924b401ca9
Devang Tyagi <devangtyagi2@gmail.com>
2018-06-06 00:48:36 +0530

    ch
    cg

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http= require('http');
 const app=require('./app');
-const port =process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+const port =process.env.OPENSHIFT_NODEJS_PORT;
+const address = process.env.OPENSHIFT_NODEJS_IP;
 
 const server= http.createServer(app);
 server.listen(port,address);
-console.log("server is listening to "+address +port);
+console.log("server is listening to "+address +" "+ +port);
