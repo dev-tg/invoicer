3d35791ad1f53b57ff059c744145f51be65ce9bb
Devang  Tyagi <dev@Devangs-MacBook.local>
2018-06-09 00:51:32 +0530

    ert

--- app.js
+++ app.js
@@ -8,27 +8,8 @@ const morgan = require("morgan");
 // middlewares
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
-// // mongoose.connect('mongodb://devangtyagi:Cryptonite_11@galaxy-invoicer-shard-00-00-oo0f9.mongodb.net:27017,galaxy-invoicer-shard-00-01-oo0f9.mongodb.net:27017,galaxy-invoicer-shard-00-02-oo0f9.mongodb.net:27017/test?ssl=true&replicaSet=galaxy-invoicer-shard-0&authSource=admin&retryWrites=true');
-console.log(process.env);
-// if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-//     var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-        mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-        mongoUser = process.env[mongoServiceName + '_USER'];
-  
-//     if (mongoHost && mongoPort && mongoDatabase) {
-//       mongoURLLabel = mongoURL = 'mongodb://';
-//       if (mongoUser && mongoPassword) {
-        mongoURL += mongoUser + ':' + mongoPassword + '@';
-//       }
-//       // Provide UI label that excludes user id and pw
-//       mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-      mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-      console.log(mongoURL);
-  
-//     }}
+mongoose.connect('mongodb://devangtyagi:Cryptonite_11@galaxy-invoicer-shard-00-00-oo0f9.mongodb.net:27017,galaxy-invoicer-shard-00-01-oo0f9.mongodb.net:27017,galaxy-invoicer-shard-00-02-oo0f9.mongodb.net:27017/test?ssl=true&replicaSet=galaxy-invoicer-shard-0&authSource=admin&retryWrites=true');
+
 //CORS HANDLER
 app.use(morgan("dev"));  
 app.use((req,res,next)=>{
